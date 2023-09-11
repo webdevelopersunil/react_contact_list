7c7803b3d61a6b4f246fa5f3555e4cca349dc856
webdevelopersunil <he.kingsmen@gmail.com>
2023-09-11 22:33:16 +0530

    React Contact List

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = props => {
+  return (
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-3">
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          {props.header}
+        </Link>
+
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              <i className="fa fa-home"></i> Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/contact/add">
+              <i className="fa fa-plus"></i> Add
+            </Link>
+          </li>
+          {/* <li className="nav-item">
+            <Link className="nav-link" to="/about">
+              <i className="fa fa-question"></i> About
+            </Link>
+          </li> */}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+Header.defaultProps = {
+  header: "Contact Manager"
+};
+
+export default Header;
