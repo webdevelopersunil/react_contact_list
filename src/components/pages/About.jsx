7d5762213ab1f39d0b74f2a362b206db7225c02b
webdevelopersunil <he.kingsmen@gmail.com>
2023-09-13 23:38:08 +0530

    relaced the about page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
     <div>
       <h1 className="display-4">About Contact Manager</h1>
       <p className="lead">Simple React App to manage contacts</p>
-      <p className="secondary">Version 1.0.0</p>
+      <p className="secondary">Version 1.2.0</p>
     </div>
   );
 }
